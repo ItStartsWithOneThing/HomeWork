99d1fb421d2ad18ade05a2ecc4670eb5c82241c4
Konstantin Turkin <like69.kt@gmail.com>
2022-02-18 21:32:27 +0200

    Добавьте файлы проекта.

---
+++ HomeWork/Controllers/LaptopController.cs
@@ -0,0 +1,159 @@
+﻿using HomeWork.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LaptopController : ControllerBase
+    {
+        private static List<Laptop> _laptops;
+        private readonly ILogger<LaptopController> _logger;
+
+        static LaptopController()
+        {
+            _laptops = new List<Laptop>();
+
+        }
+
+        public LaptopController(ILogger<LaptopController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        #region GetMethods
+
+        [HttpGet("getAll")] // Without Params
+        public IActionResult GetAllLaptops()
+        {
+            if (_laptops != null)
+            {
+                _logger.LogInformation("GetAllLaptops: Successfully taken");
+                return Ok(_laptops);
+            }
+
+            _logger.LogInformation("GetAllLaptops: Couldnt find any");
+            return NotFound();
+        }
+
+        [HttpGet] // Three Params From Querry
+        public IActionResult GetByMultiple([FromQuery] int ram, [FromQuery] int ssd, [FromQuery] string model)
+        {
+            var dbLaptop = _laptops.FirstOrDefault(x => x.Ram == ram && x.Ssd == ssd && x.Model == model);
+
+            if (dbLaptop != null)
+            {
+                _logger.LogInformation($"GetByMultiple: Laptop finded by id - {dbLaptop.Id}");
+                return Ok(dbLaptop);
+            }
+
+            _logger.LogInformation($"GetByMultiple: Laptop with RAM - {ram}, SSD - {ssd}, Model - {model} is not founded");
+            return NotFound();
+        }
+
+        [HttpGet("{id}")] // One Parameter
+        public IActionResult GetById(Guid id)
+        {
+            var dbLaptop = _laptops.FirstOrDefault(x => x.Id == id);
+
+            if (dbLaptop != null)
+            {
+                _logger.LogInformation($"GetById: Laptop finded by id - {dbLaptop.Id}");
+                return Ok(dbLaptop);
+            }
+
+            _logger.LogInformation($"GetById: Laptop with id - {id} is not founded");
+            return NotFound($"{id}");
+        }
+
+        [HttpGet("{minRam}/{maxRam}")] // Getting Range By Two Params with BackSlash "/"
+        public IActionResult GetRangeByRam(int minRam, int maxRam)
+        {
+            if (minRam < 4 || maxRam > 64)
+            {
+                _logger.LogInformation($"GetRangeByRam: Incorrect input Min-{minRam}, Max-{maxRam}");
+                return BadRequest("Неверный ввод. Диагональ дисплея должна быть в диапазоне 11-18'");
+            }
+
+            var dbLaptops = _laptops.Where(x => minRam <= x.Ram && x.Ram <= maxRam);
+
+            if (dbLaptops == null)
+            {
+                _logger.LogInformation($"GetRangeByRam: Result-NotFound. Input Min-{minRam}, Max-{maxRam}");
+                return NotFound($"Подходящие ноуты не найдены");
+            }
+
+            _logger.LogInformation($"GetRangeByRam: Valid request. Min-{minRam}, Max-{maxRam}");
+            return Ok(dbLaptops);
+        }
+
+        #endregion
+
+        #region PostMethod
+
+        [HttpPost]
+        public IActionResult AddNewLaptop(Laptop laptop)
+        {
+            laptop.Id = Guid.NewGuid();
+
+            if(laptop.Price != 0)
+            {
+                laptop.Description = "В продаже";
+            }
+            else
+            {
+                laptop.Description = "Цена формируется";
+            }
+
+            _laptops.Add(laptop);
+            _logger.LogInformation($"AddNewLaptop: Added new Laptop with id-{laptop.Id}");
+            return Ok(laptop);
+        }
+
+        #endregion
+
+        #region DeleteMethod
+        [HttpDelete("{id}")]
+        public IActionResult DeleteLaptop(Guid id)
+        {
+            var dbLaptop = _laptops.FirstOrDefault(x => x.Id == id);
+
+            if(dbLaptop != null)
+            {
+                _laptops.Remove(dbLaptop);
+                _logger.LogInformation($"DeleteLaptop: deleted Laptop with id-{dbLaptop.Id}");
+                return NoContent();
+            }
+
+            _logger.LogInformation($"DeleteLaptop: Laptop wit id-{id} is not found");
+            return NotFound();
+        }
+        #endregion
+
+        #region PutMethod
+        [HttpPut]
+        public IActionResult UpdateLaptop(Laptop laptop)
+        {
+            var dbLaptop = _laptops.FirstOrDefault(x => x.Id == laptop.Id);
+
+            if(dbLaptop != null)
+            {
+                var index = _laptops.IndexOf(laptop);
+                _laptops[index] = laptop;
+                return Ok(laptop);
+            }
+
+            _logger.LogInformation($"UpdateLaptop: Laptop wit id-{laptop.Id} is not found");
+            return NotFound();
+        }
+        #endregion
+
+
+    }
+}
---
+++ HomeWork/Models/Laptop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork.Models
+{
+    public class Laptop
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Поле обязательно к заполнению")]
+        [MinLength(2)]
+        [MaxLength(28)]
+        public string Model { get; set; }
+
+        [Required(ErrorMessage = "Поле обязательно к заполнению")]
+        [Range(4, 64, ErrorMessage = "Объем RAM должен быть в диапазоне 4-64Гб")]
+        public int Ram { get; set; }
+
+        [Required(ErrorMessage = "Поле обязательно к заполнению")]
+        [Range(128, 1000, ErrorMessage = "Объем SSD должен быть в диапазоне 128-1000Гб")]
+        public int Ssd { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Поле обязательно к заполнению")]
+        [Range(11, 18, ErrorMessage = "Диагональ дисплея должна быть в диапазоне 11-18'")]
+        public double Display { get; set; }
+
+        public string Description { get; set; }
+    }
+}
